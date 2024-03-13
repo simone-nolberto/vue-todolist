57169603345333e0f382cf105720cfe9d375bd48
simone-nolberto <simone.nolberto@outlook.com>
2024-03-13 16:51:34 +0100

    update with bonus 2

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,8 +29,8 @@ createApp({
     methods: {
         addTask() {
             // if (this.newTask.text.length > 3) {
-            //     this.tasks.unshift(this.newTask.text);
-            //     this.newTask.text = '';
+            //     
+            //     
             //     this.error = '';
 
             // } else {
@@ -39,21 +39,30 @@ createApp({
             // }
 
             console.log(this.newTask.text);
-            
-            function Task(text, done) {
-                this.text = this.newTask.text;
-                this.done = false;
-            }
 
-            const newTodo = new Task(this.newTask.text, false);
+            this.tasks.unshift(this.newTask.text, false);
+            this.newTask.text = '';
+
+            // function task(text, done) {
+            //     this.text = this.newTask.text;
+            //     this.done = false;
+            // }
+
+            // const newTodo = new task(this.newTask.text, false);
+            // this.tasks.unshift(newTodo)
 
 
         },
 
         remove(taskId) {
             this.tasks.splice(taskId, 1)
-        }
+        },
 
+        check(todo) {
+            console.log(todo.done);
+            todo.done = !todo.done;
+
+        },
 
     }
 }).mount('#app')
